9139177c0d07df27a48080e5bef018186175a0fe
nadir <nadirlaskar@gmail.com>
2019-11-01 03:09:12 +0530

    removed empty lines

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -8,7 +8,6 @@ function Counter({count, desc, onChange}){
       <div className="Counter-box">{count}</div>
       <div className="Counter-box Counter-button" onClick={()=>onChange(count+1)}>+</div>
       <div className="Counter-desc">{desc}</div>
-
     </div>
   );
 }
