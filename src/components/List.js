73ac62ccb44d803ae12ee9ca141a386a13adeddd
nadir <nadirlaskar@gmail.com>
2019-11-01 10:53:15 +0530

    updated list item

--- src/components/List.js
+++ src/components/List.js
@@ -1,22 +1,25 @@
 import React from "react";
 import "./List.css";
 
-function ListItem({ selected, id, item, onSelect }) {
-  return (
-    <li
-      className={selected === id ? "List-selected-item" : null}
-      key={id}
-      onClick={() => onSelect(id, item)}
-    >
-      {item.name}
-    </li>
-  );
+function ListItem({ id, item }) {
+  return item.name;
 }
-function List({ items, selected = 0, onSelect, ItemRenderer = ListItem }) {
+function List({ items, selected = null, onSelect, ItemRenderer = ListItem }) {
   return (
     <ul className="List-main">
       {items.map((item, id) => {
-        return <ItemRenderer id={id} selected={selected} item={item} onSelect={onSelect} />
+        return (
+          <li
+            className={selected === id ? "List-selected-item" : null}
+            key={id}
+            onClick={() => onSelect(id, item)}
+          >
+            <ItemRenderer
+              id={id}
+              item={item}
+            />
+          </li>
+        );
       })}
     </ul>
   );
