073cc8d1aaa3377ebcd1de22d0e110f89d7e8f2e
nadir <nadirlaskar@gmail.com>
2019-11-01 14:47:22 +0530

    added step

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Counter.css';
 
-function Counter({count, desc, onChange}){
+function Counter({count, desc, onChange, step=1}){
   return(
     <div className="Counter-wrapper">
-      <div className="Counter-box Counter-button" onClick={()=>onChange(count-1)}>-</div>
+      <div className="Counter-box Counter-button" onClick={()=>onChange(count-step)}>-</div>
       <div className="Counter-box">{count}</div>
-      <div className="Counter-box Counter-button" onClick={()=>onChange(count+1)}>+</div>
+      <div className="Counter-box Counter-button" onClick={()=>onChange(count+step)}>+</div>
       <div className="Counter-desc">{desc}</div>
     </div>
   );
