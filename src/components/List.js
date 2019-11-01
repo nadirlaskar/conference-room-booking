b71da3d1af57f114848ef79ce91e28701788c8bc
nadir <nadirlaskar@gmail.com>
2019-11-01 10:45:22 +0530

    added Custom Renderer for ListItem

--- src/components/List.js
+++ src/components/List.js
@@ -1,18 +1,23 @@
 import React from "react";
 import "./List.css";
 
-function List({ items, selected = 0, onSelect }) {
+function ListItem({ selected, id, item, onSelect }) {
+  return (
+    <li
+      className={selected === id ? "List-selected-item" : null}
+      key={id}
+      onClick={() => onSelect(id, item)}
+    >
+      {item.name}
+    </li>
+  );
+}
+function List({ items, selected = 0, onSelect, ItemRenderer = ListItem }) {
   return (
     <ul className="List-main">
-      {items.map((x, id) => (
-        <li
-          className={selected === id ? "List-selected-item" : null}
-          key={id}
-          onClick={() => onSelect(id, x)}
-        >
-          {x.name}
-        </li>
-      ))}
+      {items.map((item, id) => {
+        return <ItemRenderer id={id} selected={selected} item={item} onSelect={onSelect} />
+      })}
     </ul>
   );
 }
